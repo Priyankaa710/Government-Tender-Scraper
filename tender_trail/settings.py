1457f8f316f42c89a60d1133309ffcad874f3cfe
Priyanka Nagare <pnagare2004@gmail.com>
2026-07-26 14:00:22 +0530

    Update MongoDB Atlas settings

--- tender_trail/settings.py
+++ tender_trail/settings.py
@@ -84,11 +84,8 @@ DATABASES = {
 }
 
 # --- MongoDB: primary store for tender documents (via MongoEngine) ---------
-MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
-MONGO_PORT = int(os.getenv("MONGO_PORT", "27017"))
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "tender_trail")
-MONGO_USERNAME = os.getenv("MONGO_USERNAME") or None
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD") or None
+
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 
