6885be80374c5ee1342d95e48e4a428b33aa7893
Priyanka Nagare <pnagare2004@gmail.com>
2026-07-26 13:17:03 +0530

    Updated project

--- tenders/apps.py
+++ tenders/apps.py
@@ -5,3 +5,6 @@ class TendersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tenders"
     verbose_name = "Tenders"
+    
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", debug=True)
